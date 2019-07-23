bf8975ef69536c70e36ff5338e1df4e96d94d84d
Amoodaa <amoodaa@gmail.com>
2019-07-23 13:39:27 +0300

    Initial commit installed eslint, npm packages, and .gitignore relates #1 and started file structure

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const router = require('./router');
+
+let port = process.env.PORT;
+if (port === '' || port === undefined) {
+  port = 8000;
+}
+const server = http.createServer(router);
+// eslint-disable-next-line no-console
+server.listen(port, () => console.log(`server listenting on http://localhost:${port}`));
