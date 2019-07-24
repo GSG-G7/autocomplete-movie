2b2d72947395f87ec551cdf8e7349fe76f530e5f
Amoodaa <amoodaa@gmail.com>
2019-07-24 15:55:32 +0300

    dom search button relates #29

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-undef */
 const selector = id => document.querySelector(`#${id}`);
 const searchInput = selector('search-input');
 const searchSection = selector('search-section');
-
+const createE = tag => document.createElement(tag);
 const callback = (res) => {
   const suggestedTitles = res.results.slice(0, 10);
   const suggestions = selector('suggestions');
@@ -29,4 +30,53 @@ searchInput.addEventListener('keyup', (e) => {
     const newRes = titlesArray(inputValue, array);
     callback(newRes);
   }
+});
+
+const createMovieNode = (obj) => {
+  const div = createE('div');
+  const imgDiv = createE('div');
+  const textDiv = createE('div');
+  const title = createE('h2');
+  const img = createE('img');
+  const overview = createE('p');
+  const genre = createE('p');
+  const ratingDiv = createE('div');
+  const rating = createE('span');
+  const ratingIco = createE('i');
+
+  rating.textContent = ` ${obj.rating}`;
+  title.textContent = obj.name;
+  img.src = obj.posterLink;
+  overview.textContent = obj.overview;
+  genre.textContent = `Genre: ${obj.genre.join(', ')}`;
+
+  ratingIco.classList.add('fas', 'fa-star', 'star-color');
+  imgDiv.classList.add('result-movie-imageDiv');
+  textDiv.classList.add('result-movie-textDiv');
+
+  ratingDiv.appendChild(ratingIco);
+  ratingDiv.appendChild(rating);
+  textDiv.appendChild(title);
+  textDiv.appendChild(ratingDiv);
+  textDiv.appendChild(overview);
+  textDiv.appendChild(genre);
+  imgDiv.appendChild(img);
+
+  div.appendChild(imgDiv);
+  div.appendChild(textDiv);
+  div.classList.add('result-movieNode');
+
+  return div;
+};
+
+selector('search-btn').addEventListener('click', () => {
+  const { id } = array.results.find(e=> e.title === searchInput.value);
+  generateXhr(`/get?q=${id}`, (res) => {
+    console.log(extractDetails(res));
+    const container = createE('div');
+    container.id = 'result-movie-container';
+    const oldContainer = selector('result-movie-container');
+    container.appendChild(createMovieNode(extractDetails(res)));
+    selector('result-movie').replaceChild(container, oldContainer);
+  });
 });
