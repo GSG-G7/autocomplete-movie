236e0384be1852608eebf941ee436d5b217f8641
SajedaIsmail <eng.sajedaismail@gmail.com>
2019-07-24 15:40:40 +0300

    Add style to input field edis on dom un comment env for port relates #18

--- public/js/dom.js
+++ public/js/dom.js
@@ -3,30 +3,31 @@ const searchInput = selector('search-input');
 const searchSection = selector('search-section');
 
 const callback = (res) => {
-  const suggestedTitles = res.results.slice(0, 10);
+  const suggestedTitles = res.results;
   const suggestions = selector('suggestions');
   const newDatalist = document.createElement('datalist');
   suggestedTitles.forEach((ele) => {
     const option1 = document.createElement('option');
     option1.value = ele.title;
+    option1.classList.add('')
     newDatalist.appendChild(option1);
   });
   newDatalist.classList.add = 'suggestions';
   newDatalist.id = 'suggestions';
   searchSection.replaceChild(newDatalist, suggestions);
 };
-let array;
+// let array;/
 searchInput.addEventListener('keyup', (e) => {
   const inputValue = e.target.value;
 
-  if (inputValue.length === 1) {
-    // eslint-disable-next-line no-undef
-    generateXhr(`/find?q=${inputValue}`, (res) => {
-      array = res;
-      callback(array);
-    });
-  } else {
-    const newRes = titlesArray(inputValue, array);
-    callback(newRes);
-  }
+  // if (inputValue.length === 1) {
+  // eslint-disable-next-line no-undef
+  generateXhr(`/find?q=${inputValue}`, (res) => {
+    // array = res;
+    callback(res);
+  });
+  // } else {
+  //   const newRes = titlesArray(inputValue, array);
+  //   callback(newRes);
+  // }
 });
