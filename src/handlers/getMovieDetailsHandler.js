723f2ce97813c1ad5c948a90a734d138e7704059
Amoodaa <amoodaa@gmail.com>
2019-07-24 14:30:49 +0300

    implemented the function relates #24 and used the api key from enviroment vars and used request npm package to make api requests on server relates #22

---
+++ src/handlers/getMovieDetailsHandler.js
@@ -0,0 +1,15 @@
+const request = require('request');
+const getMime = require('../getMime');
+
+const moviedbDetails = id => `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.MOVIESDB}&language=en-US`;
+
+module.exports = (req, res) => {
+  request(moviedbDetails(req.url.slice(7)), (err, response, body) => {
+    if (err) {
+      res.writeHead(404, getMime('html'));
+      res.end('<h1>Not Found 404<h1>');
+    }
+    res.writeHead(response.statusCode, getMime('json'));
+    res.end(body);
+  });
+};
