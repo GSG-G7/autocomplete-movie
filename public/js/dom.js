76b60ff754d137e3a9fb46d051eab98893813a38
FadiAlamassi <alamassifadi@gmail.com>
2019-07-24 12:03:46 +0300

    create a function to filter the server respons and return the title that starts with the user input create datalist options to show autocomplete suggestions replace the old datalist with a new one with each event Relates #18

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,21 +1,37 @@
-// const fetchTitles = require('./logic.js');
-
 const selector = id => document.querySelector(`#${id}`);
-const suggestions = selector('suggestions');
-const input = selector('search-input');
+const searchInput = selector('search-input');
+const searchSection = selector('search-section');
 
-input.addEventListener('keydown', (e) => {
+const titlesArray = (input, result) => {
+  const title = result.results.filter(ele => ele.title.startsWith(input));
+  return title;
+};
+
+searchInput.addEventListener('keyup', (e) => {
   const inputValue = e.target.value;
-  generateXhr(`/find?q=${inputValue}`, () => {
 
-  });
-  extractTitles(inputValue, Obj);
-  const option1 = document.createElement('option');
-  option1.value = e.target.value;
-  suggestions.appendChild(option1);
-//   console.log();
+  const callback = (res) => {
+    const suggestedTitles = titlesArray(inputValue, res);
+    const suggestions = selector('suggestions');
+    const newDatalist = document.createElement('datalist');
+    console.log(suggestedTitles);
+    suggestedTitles.forEach((ele) => {
+      const option1 = document.createElement('option');
+      option1.value = ele.title;
+      newDatalist.appendChild(option1);
 
+      //   suggestions.removeChild(option1);
+    });
+    newDatalist.classList.add = 'suggestions';
+    newDatalist.id = 'suggestions';
+    searchSection.replaceChild(newDatalist, suggestions);
+  };
 
-//   fetchTitles(inputValue, object);
+  const test = { results: [{ id: 650, title: 'Boyz n the Hood' }, { id: 661, title: 'Boyz ukmukjfyh' }, { id: 662, title: 'Boyz kdtg djfh ' }] };
+  callback(test);
 
+  // eslint-disable-next-line no-undef
+  //   generateXhr(`/find?q=${inputValue}`, callback);
+
+  //   extractTitles(inputValue, Obj);
 });
